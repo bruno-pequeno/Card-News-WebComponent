174526d56738038ddb58bda0cf516f919c152a43
Bruno Pequeno <99445630+BrunoPequeno@users.noreply.github.com>
2023-09-13 18:25:48 +0000

    Creating component CardNews

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -3,7 +3,40 @@ class Cardnews extends HTMLElement {
         super();
 
         const shadow = this.attachShadow({mode: "open"})
-        shadow.innerHTML = "<h1> Hello </h1>";
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
+
+    build(){
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        componentRoot.setAttribute("class", "card_left");
+
+        const autor = document.createElement("span");
+        const linkTitle = document.createElement("h1");
+        const newsContent = document.createElement("p");
+
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+
+        const cardRight = document.createElement("div");
+        componentRoot.setAttribute("class", "card_right");
+
+        const newImage = document.createElement("img");
+        cardRight.appendChild(newImage);
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
+
+    style() {
+
     }
 }
 
