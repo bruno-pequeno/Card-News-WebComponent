c1bde578b78d2be2513131c3f5eef49f06b584e0
Bruno Pequeno <99445630+BrunoPequeno@users.noreply.github.com>
2023-09-13 18:10:12 +0000

    Creating dinamic components

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -1,10 +1,10 @@
-class CardNews extends HTMLElement {
-    constructor (){
+class Cardnews extends HTMLElement {
+    constructor() {
         super();
 
-        const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1> Hello Bruno! </h1>";
+        const shadow = this.attachShadow({mode: "open"})
+        shadow.innerHTML = "<h1> Hello </h1>";
     }
 }
 
-customElements.define('card-news', CardNews)
+customElements.define("card-news", Cardnews);
